c6f238a5b46cdc04f3b2ea69b7ec86d11ed69a65
AntonFriis <anlf@itu.dk>
2021-09-03 15:20:37 +0200

    Added BDSA directory and created first C# app

---
+++ BDSA/Lecture 1.1/HelloWorld.Tests/HelloWorldTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+using Xunit;
+
+namespace HelloWorld.Tests
+{
+    public class HelloWorldTest
+    {
+        [Fact]
+        public void Main_prints_Hello_World()
+        {
+            // Arrange
+            var writer = new StringWriter();
+            
+            Console.SetOut(writer);
+            
+            // Act
+            HelloWorld.Main(new string[0]);
+            
+            // Assert
+            var output = writer.GetStringBuilder().ToString().Trim();
+            Assert.Equal("Hello World!", output);
+        }
+    }
+}
